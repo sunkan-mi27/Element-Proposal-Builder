57797ab157659c82e2864f0b07ef263d045cd408
sunkanmi <iolasunkanmi405@gmail.com>
2026-07-30 06:20:36 +0100

    Working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import "../styles/home.css";
 
 const Home = () => {
   return (
@@ -7,9 +8,16 @@ const Home = () => {
       <Navbar />
       <Hero />
 
-      <main>
-        <h1>Proposal Builder</h1>
-        <p>Create proposals that win clients.</p>
+      <main className="home-intro">
+        <h1 className="hero-title">
+          <span className="proposal-text">Proposal</span>
+          {""}
+          <span className="builder-text"> Builder</span>
+        </h1>
+        <p>
+          Create proposals that close <strong>deals</strong> for modern
+          businesses
+        </p>
       </main>
     </>
   );
